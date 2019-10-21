9daec1bf077657f8e06e009b6c59ea43edf4777a
xmaten <xmaten13@gmail.com>
2019-10-21 20:33:48 +0200

    Prepare basic MVC structure (#1)
    * Prepare basic Model class
    
    * Prepare basic View class
    
    * Prepare basic Controller class
    
    * Connect MVC

---
+++ src/js/model.js
@@ -0,0 +1,15 @@
+class Model {
+  constructor(data) {
+    this.data = data;
+  }
+
+  getData() {
+    return this.data;
+  }
+
+  setData(newData) {
+    this.data = newData;
+  }
+}
+
+export default Model;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
 import './styles/main.css';
+import Model from './js/model';
+import View from './js/view';
+import Controller from './js/controller';
+
+const data = [];
+const model = new Model(data);
+const view = new View();
+
+const controller = new Controller(model, view);
 
 // Please use https://open-platform.theguardian.com/documentation/
